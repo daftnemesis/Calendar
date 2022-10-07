25e8ea0ca5dbfbee5e154c2b924cf0ec037c7cc6
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-10-06 22:27:08 -0500

    Terminando de estilizar la LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,46 +1,27 @@
 //Pagina principal para el login y el register ya que estaran en la misma pagina
 export const LoginPage = () => {
   return (
-    <div className="grid content-center w-10/12 mx-auto h-screen text-lg">
-      <div className="flex justify-evenly">
-        <div className="text-center shadow-lg w-2/4 rounded-lg border border-gray-00">
-          <h3 className="text-2xl font-bold my-5">Ingreso</h3>
-          <form action="">
-            <div>
-              <input type="text" placeholder="Correo" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:outline-blue-500  m-2 px-2 py-1" />
-            </div>
-            <div>
-              <input type="text" placeholder="Contrasena" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:outline-blue-500  m-2 px-2 py-1" />
-            </div>
-            <div className="">
-              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center" >
-                Login
-              </button>
-            </div>
+    <div className="flex justify-center items-center w-full h-screen ">
+      <div className="flex w-full justify-center">
+        <div className="flex flex-col w-2/6 items-center border border-gray-200 rounded-xl shadow-xl ">
+          <h3 className="font-bold text-2xl tracking-normal my-10">Login</h3>
+          <form action="" className="flex flex-col px-5 w-5/6 lg:w-4/6">
+            <input type="text" placeholder="Email" className="my-1 px-2 border border-gray-200 rounded-lg focus:outline-blue-500 text-base py-1" />
+            <input type="password" placeholder="Password"  className="my-1 px-2 border border-gray-200 rounded-lg focus:outline-blue-500 text-base py-1" />
+            <input type="submit" value='Login' className="bg-blue-500 text-white font-bold rounded-xl sm:w-fit px-5 py-1 self-center mt-2" />
           </form>
-        </div>
 
-        <div className="text-center shadow-lg w-2/4 rounded-lg bg-blue-500">
-          <h3>Registro</h3>
-            <form action="">
-              <div>
-                <input type="text" placeholder="Nombre" />
-              </div>
-              <div>
-                <input type="text" placeholder="Correo" />
-              </div>
-              <div>
-                <input type="text" placeholder="Contrasena" />
-              </div>
-              <div>
-                <input type="text" placeholder="Repita la contrasena" />
-              </div>
-              <div>
-                <button type="submit">
-                  Crear Cuenta
-                </button>
-              </div>
-            </form>
+        </div>
+        <div className="flex flex-col w-2/6 items-center bg-blue-500 rounded-xl shadow-2xl">
+          <h3 className="font-bold text-2xl tracking-normal my-10 text-white">Registro</h3>
+          <form action="" className="flex flex-col px-5 w-5/6 lg:w-4/6">
+            <input type="text" placeholder="Name" className="my-1 px-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-cyan-500 text-base py-1" />
+            <input type="text" placeholder="Last Name"  className="my-1 px-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-cyan-500 text-base py-1" />
+            <input type="text" placeholder="Email" className="my-1 px-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-cyan-500 text-base py-1" />
+            <input type="password" placeholder="Password" className="my-1 px-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-cyan-500 text-base py-1" />
+            <input type="password" placeholder="Confirm Password" className="my-1 px-2 border border-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-cyan-500 text-base py-1" />
+            <input type='submit' value='Crear cuenta' className="bg-white text-blue-500 font-bold rounded-xl sm:w-fit px-5 py-1 self-center mb-10 mt-2" />
+          </form>
         </div>
       </div>
     </div>
