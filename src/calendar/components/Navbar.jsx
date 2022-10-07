6b00c49ddd0557240fbd604fb5a94dfd53e54c6c
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-10-06 23:00:58 -0500

    Creando el Navbar de la app Tambien dandole estilos y agregandola a la CalendarPage

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full sticky top-0 '>
+      <nav className='w-full bg-indigo-800 mb-2 px-4 py-2 shadow-lg'>
+        <ul className='flex justify-between text-white items-center'>
+          <li>
+            <span>
+              <i className='fas fa-calendar-alt'></i>
+              &nbsp;
+              Jose Lozano
+            </span>
+          </li>
+          <li>
+            <button className='bg-red-700 rounded-lg px-3 focus:ring-2 focus:ring-red-600'>
+              <span>
+                <i className='fas fa-sign-out-alt'></i>
+                &nbsp;
+                Salir
+              </span>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,52 @@
+import { Navbar } from "../"
+
 //Pagina de nuestra aplicacion principal
 export const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
+    <>
+      <Navbar />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, libero recusandae sequi harum atque et quas esse labore assumenda eum voluptas, culpa suscipit provident natus consequuntur cupiditate sunt numquam.
+
+    </>
   )
 }
