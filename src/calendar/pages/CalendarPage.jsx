4c13a36ed079c5c80538105be51d7fb8b0ad80fc
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-10-13 22:02:46 -0500

    agregando el localizer en su propio helper y refactorizando el CalendarPage

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,23 +1,11 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
 
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns/esm'
-import enUS from 'date-fns/locale/en-US'
+import { addHours } from 'date-fns/esm'
+import { getMessagesES, localizer } from '../../helpers'
 
 import { Navbar } from "../"
 
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
 
 const events = [{
   title: 'Cumpleanos de faby',
@@ -34,17 +22,35 @@ const events = [{
 //Pagina de nuestra aplicacion principal
 export const CalendarPage = () => {
 
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({event, start, end, isSelected})
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+
+  }
   
   return (
     <>
       <Navbar />
       
       <Calendar
+        culture='es'
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 48px)' }}
+        messages = {getMessagesES()}
+        eventPropGetter={eventStyleGetter}
       />
 
     </>
